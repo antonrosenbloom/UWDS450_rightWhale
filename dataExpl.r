99a706a99ac4e0b1bf6ed3cd514b41c9e0790084
antonrosenbloom <anton.rosenbloom@gmail.com>
2015-11-14 20:56:20 -0800

    Part1.docx

--- dataExpl.r
+++ dataExpl.r
@@ -6,6 +6,17 @@ summary(whales_train)
 
 #Length of the training dataset
 nrow(whales_train)
+dim(whales_train)
 
 ## Number of individual whales in the training set
 length(unique(whales_train$whaleID))
+
+#Any missing data?
+whales_train[is.na(whales_train)]
+#Nope
+
+
+
+
+
+
